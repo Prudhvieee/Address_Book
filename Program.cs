33572373e3d14852b15aeb6db082e360d0f2a930
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-11 19:53:30 +0530

    [Prudhvi] code refractored to edit contact

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace Address_Book
 {
     public class Program
@@ -7,32 +6,8 @@ namespace Address_Book
         static void Main(string[] args)
         {
             Console.WriteLine("---Welcome to Address Book---");
-            Console.WriteLine("***Enter Your Choice***");
-            Console.WriteLine("1.Add Details");
-            Console.WriteLine("2.Display Details");
-            Console.WriteLine("3.Edit contact");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            addDetails details = new addDetails();
-            switch (choice)
-            {
-                case 1:
-                    details.AddContact();
-                    long number = Convert.ToInt32(Console.ReadLine());
-
-                    details.EditContact(number);
-
-                    break;
-                case 2:
-                    details.displayAddressBook();
-                    break;
-                case 3:
-                    Console.WriteLine("Enter the phone number of the person you want to edit");
-                   // long number = Convert.ToInt32(Console.ReadLine());
-                    //details.EditContact(number);
-                    break;
-                default:
-                    break;
-            }
+            addDetails newDetails = new addDetails();
+            newDetails.DisplayMenu();
         }
     }
 }
